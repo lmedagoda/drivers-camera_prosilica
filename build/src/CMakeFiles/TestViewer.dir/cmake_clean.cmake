d4624b53bb5826439c70703cd31cd91993008383
Alexander Duda <Alexander.Duda@dfki.de>
2010-03-02 09:13:39 +0100

    changed config

---
+++ build/src/CMakeFiles/TestViewer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/TestViewer.dir/TestViewer.cpp.o"
+  "../bin/TestViewer.pdb"
+  "../bin/TestViewer"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/TestViewer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
